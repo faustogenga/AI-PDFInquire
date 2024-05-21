f8ba37351f955e73ce925c8d55ad91dee7a72d77
faustogengaalfaro@gmail.com <faustogengaalfaro@gmail.com>
2024-05-21 13:25:02 +0200

    fix routing for deployment

--- react_frontend/src/views/HomePage.jsx
+++ react_frontend/src/views/HomePage.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import ChatBody from "../components/ChatBody";
 
 const HomePage = () => {
-
+    // Render the HomePage component
     return (
         <>
             <div className="home container-fluid">
