15082e6aa07bb4e3346fc578b34fbea3e50abfe8
faustogengaalfaro@gmail.com <faustogengaalfaro@gmail.com>
2024-05-21 12:41:02 +0200

    update main hosting port for keyobe

--- my_app/main.py
+++ my_app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 # Include the routes from the router
 app.include_router(router)
 
-# Define the main function to run the FastAPI application
+# Define the main function to run the FastAPI application for HOSTING IN KEYOBE
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
